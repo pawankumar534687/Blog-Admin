520524c0798c342dfe7be0373c9703161e11d6b6
pawankumar534687 <pawankumar534687@gmail.com>
2025-07-30 18:59:41 -1200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import EditBlog from "./components/blog/EditBlog"
+import { Routes, Route, Navigate  } from "react-router-dom";
+import EditBlog from "./components/blog/EditBlog";
 import Dashboard from "./components/Dashboard";
 import CreateBlog from "./components/blog/CreateBlog";
 import ManageBlog from "./components/blog/ManageBlog";
-
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route
-          path="/"
-          element={
-           
-              <Dashboard />
-           
-          }
-        >
+        <Route path="/" element={<Navigate to="/manage-blog" />}>
           <Route path="/manage-blog" element={<ManageBlog />} />
 
           <Route path="/create-blog" element={<CreateBlog />} />
